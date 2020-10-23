f54e7349b33b36b8c814f0ca6fa53ace8a4f4649
Callie Stump <callie@stu.mp>
2020-10-23 12:33:18 -0400

    adds elements to css, js, and html

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("form#generator").submit(function() {
+    const questionOne = parseInt($("#favDog").val());
+    const questionTwo = parseInt($("#favMovie").val());
+    const questionThree = parseInt($("#favMusic").val());
+    const questionFour = parseInt($("#favSeason").val());
+    const questionFive = parseInt($("#favRel").val());
+    const operator1 = $("#favDog").val();
+    const operator2 = $("#favMovie").val();
+    const operator3 = $("#favMusic").val();
+    const operator4 = $("#favSeason").val();
+    const operator5 = $("#favReal").val();
+    let result;
+    if 
+  }
+}
