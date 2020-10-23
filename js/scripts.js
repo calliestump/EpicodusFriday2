b80f5d975c9a8376b25455fecd4add10a65d1c50
Callie Stump <callie@stu.mp>
2020-10-23 14:02:19 -0400

    add event handles to page so answers dont refresh

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
-  $("form#generator").submit(function() {
+  let cPlus = 0
+  let ruby = 0
+  let python = 0
+  let result = ""
+  $("form#Dog").submit(function(event) {
     const questionOne = parseInt($("#favDog").val());
+    // const operator1 = $("#favDog").val();
+    if (questionOne === 1) {
+      cPlus += 1
+      print(cPlus)
+    }
+    event.preventDefault();
+  });
+  $("form#Movie").submit(function(event) {
     const questionTwo = parseInt($("#favMovie").val());
+    //const operator2 = $("#favMovie").val();
+    event.preventDefault();
+  });
+  $("form#Music").submit(function(event) {
     const questionThree = parseInt($("#favMusic").val());
+    //const operator3 = $("#favMusic").val();
+    event.preventDefault();
+  });
+  $("form#Season").submit(function(event) {
     const questionFour = parseInt($("#favSeason").val());
+    //const operator4 = $("#favSeason").val();
+    event.preventDefault();
+  });
+  $("form#Relax").submit(function(event) {
     const questionFive = parseInt($("#favRel").val());
-    const operator1 = $("#favDog").val();
-    const operator2 = $("#favMovie").val();
-    const operator3 = $("#favMusic").val();
-    const operator4 = $("#favSeason").val();
-    const operator5 = $("#favReal").val();
-    let result;
-    if 
-  }
-}
+    //const operator5 = $("#favRel").val();
+    event.preventDefault();
+  });
+  show(cPlus)
+});
