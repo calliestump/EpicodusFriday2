741c9eabc586d7102b8f252618486c21c2ce9328
Callie Stump <callie@stu.mp>
2020-10-23 18:56:44 -0400

    adds equations to forms if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,5 @@
 /*let result = ""
 
-var answer1;
-function sAnswer1(){
-  answer1 = document.getElementById('favDog').value;
-  alert(month);
-}
-var answer2;
-function sAnswer2(){
-  answer2 = document.getElementById('favMovie').value;
-  alert(month);
-}
-var answer3;
-function sAnswer3(){
-  answer3 = document.getElementById('favMusic').value;
-  alert(month);
-}
-var answer4;
-function sAnswer4(){
-  answer4 = document.getElementById('favSeason').value;
-  alert(month);
-}
-var answer5;
-function Answer5(){
-  answer5 = document.getElementById('favRel').value;
-  alert(month);
-}
-*/
 let cPlus = 0
 function cPlus2() {
   cPlus++;
@@ -39,11 +13,24 @@ let python = 0
 function python2() {
   python++;
 }
+*/
+
 $(document).ready(function() {
   $("form#generator").submit(function(event) {
-    const favDog = document.getElementById("favDog");
-    var value1 = favDog.value;
-    event.preventDefault();
+    const favDog = parseInt($("#favDog").val());
+    const favMovie = parseInt($("#favMovie").val());
+    const favMusic = parseInt($("#favMusic").val());
+    const favSeason = parseInt($("#favSeason").val());
+    const favRel = parseInt($("#favRel").val());
+    let cplus = 0;
+    //event.preventDefault();
+    if (favDog === 1) {
+        cplus += 1;
+    } else if (favDog === 2) {
+        ruby += 1;
+    } else if (favDog === 3) {
+        python +=1;
+    }
+
   });
 });
-//console.log(cPlus);
