d36ad9ed48c54c75f579ed3dd83b896aaabb2e1c
Callie Stump <callie@stu.mp>
2020-10-28 20:31:29 -0400

    adds buttons to submit answer to user and gives option to reset

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+function refreshPage() {
+  window.location.reload();
+}
 $(document).ready(function() {  
   $("form#generator").submit(function(event) {
     event.preventDefault();
@@ -9,57 +12,48 @@ $(document).ready(function() {
     let cplus = 0;
     let ruby = 0;
     let python = 0;
+
     if (favSubject == 1) {
-      cplus += 1; 
+        cplus +=1;
     } else if (favSubject == 2) {
         ruby += 1;
     } else if (favSubject == 3) {
         python +=1;
-    } else {
-      alert("Error: no user input")
-    }
-    if (favMajor == 1) {
-      cplus += 1;
+    } else if (favMajor == 1) {
+        cplus += 1;
     } else if (favMajor == 2) {
-      ruby += 1;
+        ruby += 1;
     } else if (favMajor == 3) {
-      python +=1;
-    } else {
-      alert("Error: No user input")
-    }
-    if (favMusic == 1) {
-      cplus += 1;
+        python +=1;
+    } else if (favMusic == 1) {
+        cplus += 1;
     } else if (favMusic == 2) {
-      ruby += 1;
+        ruby += 1;
     } else if (favMusic == 3) {
-      python +=1;
-    } else {
-      alert("Error: no user input")
-    }
-    if (favGame == 1) {
-      cplus += 1;
+        python +=1;    
+    } else if (favGame == 1) {
+        cplus += 1;
     } else if (favGame == 2) {
-      ruby += 1;
+        ruby += 1;
     } else if (favGame == 3) {
-      python +=1;
-    } else {
-      alert("Error: no user input")
-    }
-    if (favDance == 1) {
-      cplus += 1;
+        python +=1;  
+    } else if (favDance == 1) {
+        cplus += 1;
     } else if (favDance == 2) {
-      ruby += 1;
+        ruby += 1;
     } else if (favDance == 3) {
-      python +=1; 
-    } else {
-      alert("Error: no user input")
-    }
+        python +=1; 
+    } 
+
     if (cplus > ruby && cplus > python) {
       result = ("C+");
     } else if (ruby > python && ruby > cplus) {
+        result = ("Ruby");
     } else if (python > ruby && python > cplus) {
-      result = ("Python");
-    } 
-    alert(result)
+        result = ("Python");
+    }
+  $(".final").append(result)
   });
+  $("#submitButton").click(function() {
+  });  
 });
